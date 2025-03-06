cec5ce2e6e2070d80f44b14a45824b1a5ba219bf
Dhivya-Kalyanasundaram <dhivya.k1507@gmail.com>
2025-03-06 13:15:17 +0530

    Initial Commit

---
+++ ToDoList App/ToDoList_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoList_AppApp.swift
+//  ToDoList App
+//
+//  Created by Kalyanasundaram, Dhivya (Cognizant) on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
